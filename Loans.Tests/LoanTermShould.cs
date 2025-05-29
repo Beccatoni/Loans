978dffdbd4caa409bb4c41d8e902185686c3dc90
Rebecca Mutoni <rbccmtni1gmail.com>
2025-05-29 10:42:09 +0200

    feat: create tests for term in months

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -13,10 +13,11 @@ namespace Loans.Tests
         [Test]
         public void ReturnTermInMonths()
         {
+            // sut = system under test
             var sut = new LoanTerm(1);
 
             //check the value
-            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+            Assert.That(sut.ToMonths(), Is.EqualTo(12), "Months should be 12 * number of years");
         }
     }
 }
