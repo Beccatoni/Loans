1ded1d188ea00a40a6fcbd7f32651fabe0210700
Rebecca Mutoni <rbccmtni1gmail.com>
2025-06-11 19:00:55 +0200

    check reference types

---
+++ Loans.Tests/ProductCompareShould.cs
@@ -0,0 +1,45 @@
+using Loans.Domain.Applications;
+
+namespace Loans.Tests;
+
+public class ProductCompareShould
+{
+    [Test]
+    public void ReturnCorrectNumberOfComparisons()
+    {
+        var products = new List<LoanProduct>
+        {
+            new LoanProduct(1, "a", 1),
+            new LoanProduct(2, "b", 2),
+            new LoanProduct(3, "c", 3)
+        };
+
+        var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+        Assert.That(comparisons, Has.Exactly(3).Items);
+    }
+    
+    [Test]
+    public void NotReturnDuplicateComparisons()
+    {
+        var products = new List<LoanProduct>
+        {
+            new LoanProduct(1, "a", 1),
+            new LoanProduct(2, "b", 2),
+            new LoanProduct(3, "c", 3)
+        };
+
+        var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+        var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+        Assert.That(comparisons, Does.Contain(expectedProduct));
+        
+        Assert.That(comparisons, Has.Exactly(1)
+            .Property("ProductName").EqualTo("a")
+            .And
+            .Property("InterestRate").EqualTo(1)
+            .And
+            .Property("MonthlyRepayment").GreaterThan(0));
+    }
+}
